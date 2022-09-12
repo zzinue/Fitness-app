c18e95cd822e467ae8ec31452914ea3642359434
zzinue <zzinue@gmail.com>
2022-09-12 14:29:04 -0500

    adding more exercises

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from 'react'
-// import Pagination from '@mui/material/pagination'
+import Pagination from '@mui/material/Pagination'
 import { Box, Stack, Typography } from '@mui/material'
 import { exercisesOptions, fetchData } from '../utils/fetchData'
 import ExercisesCard from './ExercisesCard'
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
-    console.log(exercises)
+    const [currentPage, setCurrentPage] = useState(1)
+    const exercisesPerPage = 9
+    const indexOfLastExercise = currentPage * exercisesPerPage
+    const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage
+    const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise)
+    const paginate = (e, value) => {
+        setCurrentPage(value)
+        window.scrollTo({ top: 1800, behavior: 'smooth' })
+    }
     return (
         <Box id="exercises"
             sx={{ mt: { lg: '11px' } }}
@@ -20,9 +28,20 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
                     gap: { lg: '110px', xs: '50px' }
                 }}
                 flexWrap="wrap" justifyContent='center'>
-                {exercises.map((exercise, index) => (
+                {currentExercises.map((exercise, index) => (
                     <ExercisesCard key={index} exercise={exercise} />
                 ))}
+            </Stack>
+            <Stack mt="100px" alignItems="center">
+                {exercises.length > 9 && (
+                    <Pagination color="secondary" shape="rounded" defautPage={1}
+                        count={Math.ceil(exercises.length / exercisesPerPage)}
+                        page={currentPage}
+                        onChange={paginate}
+                        size="large"
+                    />
+                )}
+
             </Stack>
 
         </Box>
